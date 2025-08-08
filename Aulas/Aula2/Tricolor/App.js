eb585d9e1b261ccf75abb597d9a75404574bcec7
DarkMetter-Samuel <sajuan1020@gmail.com>
2025-08-08 10:13:46 -0300

    feat: adiciona styles e texts

--- Aulas/Aula2/Tricolor/App.js
+++ Aulas/Aula2/Tricolor/App.js
@@ -1,14 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import styles from './styles.js'
 
 export default function App() {
   return (
-    <View>
+    <View style={styles.container}>
+
+      <View style={styles.area1}>
+        <Text style={styles.title}>aaaaa</Text>
+      </View>
+
+
+      <View style={styles.area2}>
+        <Text style={styles.title}>aaaaa</Text>
+      </View>
+
+
+      <View style={styles.area3}>
+        <Text style={styles.title}>aaaaa</Text>
+      </View>
+
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-  },
-});
--- Aulas/Aula2/Tricolor/styles.js
+++ Aulas/Aula2/Tricolor/styles.js
@@ -4,5 +4,31 @@ const styles = StyleSheet.create ({
     container: {
         flex: 1,
     },
-    
-})
+    area1: {
+        flex: 1,
+        backgroundColor: '#25ab33',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    area2: {
+        flex: 1,
+        backgroundColor: '#ac45ac',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    area3: {
+        flex: 1,
+        backgroundColor: '#2245ac',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+     
+    title: {
+        fontSize: 50,
+        fontWeight: 'bold',
+        fontStyle: 'italic',
+        color: '#ac99ff'
+    }
+});
+
+export default styles;
